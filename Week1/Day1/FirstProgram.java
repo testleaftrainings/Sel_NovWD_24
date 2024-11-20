2c88a94844c5598c27f757eea3ae4bcdd2ec4890
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2024-11-20 17:25:17 +0530

    Add files via upload

---
+++ Week1/Day1/FirstProgram.java
@@ -0,0 +1,9 @@
+package week1.day1;
+
+//Acess modifier   //keyword     //ClassName
+public              class      FirstProgram {
+	
+	public static void main(String args[]) {
+		System.out.println("Welcome to TestLeaf");
+	}
+}
---
+++ Week1/Day1/LearnDatatypes.java
@@ -0,0 +1,39 @@
+package week1.day1;
+
+public class LearnDatatypes {
+
+	//Naming convention of variable
+	//one word- lowercase
+	//two word- mobileNumber - camelCase
+	
+	public static void main(String[] args) {
+		//Declaring data
+		//Age of a person
+		byte age=31;
+		
+		short age1=130;
+		
+		short amount=30000;
+		
+		int amount1=50000;
+		
+		long mobileNumber=8526511170l;
+		
+		float rateOfInterset=4.25F;
+		
+		double amount2=1.123456789;
+		System.out.println(amount2);
+		
+		float amount3=1.123456789f;
+		System.out.println(amount3);
+		
+          char logoOfTestleaf='$';
+		  char ascii=66;
+		  System.out.println(ascii);
+		  
+	    String name="Vineeth";
+		   
+		boolean doYouKnowJava =true;
+	}
+
+}
