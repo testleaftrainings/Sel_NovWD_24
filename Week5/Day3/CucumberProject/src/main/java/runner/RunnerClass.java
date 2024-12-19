048232918009155a1f071cd589ab998560bc4764
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2024-12-19 16:45:41 +0530

    Add files via upload

---
+++ Week5/Day3/CucumberProject/src/main/java/runner/RunnerClass.java
@@ -0,0 +1,11 @@
+package runner;
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
+@CucumberOptions(features="src/main/java/features/Login.feature", glue="stepdefinition", monochrome=true, publish=true)
+public class RunnerClass extends AbstractTestNGCucumberTests {
+
+}
---
+++ Week5/Day3/CucumberProject/src/test/java/features/Amazon.feature
@@ -0,0 +1,6 @@
+Feature: Login Amazon
+
+Scenario: Amazon Login
+Given Launching the browser
+
+Given Loading the url
